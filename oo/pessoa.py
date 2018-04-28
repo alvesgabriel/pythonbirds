9dd0a4fda8af924092c4a9eaa0ab00e94a73c493
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2018-04-28 20:11:40 -0300

    Criado o atributo de classe olhos da classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=28):
         self.nome = nome
         self.idade = idade
@@ -17,8 +19,13 @@ if __name__ == '__main__':
     print(gabriel.idade)
     for filho in gabriel.filhos:
         print(filho.nome)
+    gabriel.olhos = 4
     gabriel.sobrenome = 'Alves'
     print(gabriel.sobrenome)
     del gabriel.filhos
     print(gabriel.__dict__)
     print(laura.__dict__)
+    print(Pessoa.olhos)
+    print(gabriel.olhos)
+    print(laura.olhos)
+    print(id(Pessoa.olhos), id(gabriel.olhos), id(laura.olhos))
