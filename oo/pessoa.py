1b4c8f9cf8cbcc7a5e0a42cbf1aef2b1b1f79ff3
alvesgabriel <gabriel.alves.monteiro1@gmail.com>
2018-04-28 20:00:04 -0300

    Criado atributo complexo de instâcia filhos da classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,15 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=28):
+    def __init__(self, *filhos, nome=None, idade=28):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Gabriel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
+    laura = Pessoa(nome='Laura')
+    gabriel = Pessoa(laura, nome='Gabriel')
+    print(Pessoa.cumprimentar(gabriel))
+    print(id(gabriel))
+    print(gabriel.cumprimentar())
+    print(gabriel.nome)
+    print(gabriel.idade)
+    for filho in gabriel.filhos:
+        print(filho.nome)
